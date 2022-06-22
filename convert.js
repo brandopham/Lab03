42325a219a60857aca3ad343eb778a25c18446fd
brandopham <107006386+brandopham@users.noreply.github.com>
2022-06-21 23:36:12 -0500

    Add files via upload
    Was fun.

---
+++ convert.js
@@ -0,0 +1,18 @@
+function weightConvertor(numberValue){
+    document.getElementById("output").innerHTML=numberValue/2.205;
+}
+function milesConvertor(numberValue){
+    document.getElementById("output2").innerHTML=numberValue*1.60934;
+}
+function degreesConvertor(numberValue){
+    document.getElementById("output3").innerHTML=numberValue*9/5+32;
+}
+function kilogramConvertor(numberValue){
+    document.getElementById("output4").innerHTML=numberValue*2.205;
+}
+function kilometerConvertor(numberValue){
+    document.getElementById("output5").innerHTML=numberValue/1.60934;
+}
+function farConvertor(numberValue){
+    document.getElementById("output6").innerHTML=(numberValue-32)*5/9;
+}
